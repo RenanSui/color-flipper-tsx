18b59a2865ff18068171af1303cd5cf408e95276
renansui <renanddtao@gmail.com>
2022-11-12 08:27:20 -0300

    added tailwindcss and postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import './main.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
+    <div className="App bg-blue-600">
+      <h1>Hello World</h1>
+      <p className="text-3xl font-bold underline">Hello World</p>
+      <h1 className="text-xl font-medium text-yellow-200">
+    Hello world!
+  </h1>
     </div>
   )
 }
